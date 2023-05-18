d36b9798b90f738e7d70244b19f104fab159815a
Nucci-karim <nuccikarim@gmail.com>
2023-05-18 15:30:31 +0200

    primo push

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Badwords</title>
+</head>
+<body>
+    <form action="saluto.php" method="GET">
+        <label for="">Nome</label>
+        <input type="text" placeholder="Inserisci il tuo nome" name="nomePersona">
+
+        <label for="">Cognome</label>
+        <input type="text" placeholder="Inserisci il tuo cognome" name="cognomePersona">
+
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
---
+++ saluto.php
@@ -0,0 +1,15 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>risposta</title>
+</head>
+<body>
+    <h1>Ciao <?php echo $_GET['nomePersona'].' '.$_GET['cognomePersona']?> benvenuto!</h1>
+</body>
+</html>
